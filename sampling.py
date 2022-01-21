25b68bc3fb4a962de9883ebd8e3a6683a0367659
Andy Barrett <apbarret@nsidc.org>
2022-01-21 11:32:52 -0700

    add circle function

--- sampling.py
+++ sampling.py
@@ -1,1 +1,22 @@
 '''Utilities to sample files'''
+
+import numpy as np
+
+from shapely.geometry import Polygon
+
+
+def circle(x0=0, y0=0, radius=1000., npoints=360):
+    '''Returns a Shapely Polygon for a circle
+
+    :x0: x-coordinate of circle in meters
+    :y0: y-coordinate of circle in meters
+    :radius: radius of circle in meters
+    '''
+
+    x = [x0 + radius * np.cos(np.radians(theta))
+         for theta in np.linspace(0., 359., npoints)]
+    y = [y0 + radius * np.sin(np.radians(theta))
+         for theta in np.linspace(0., 359., npoints)]
+
+    return Polygon(list(zip(x, y)))
+
